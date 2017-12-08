88d65a9fa786ca00f4cddded9b46401b9525a612
taise <taise515@gmail.com>
2017-12-09 08:30:57 +0900

    Refactoring: extract fire method

--- public/js/fire.js
+++ public/js/fire.js
@@ -1,18 +1,22 @@
+const fire = function(canvas, e) {
+  e.preventDefault()
+
+  let mx = (e.pageX - canvas.offsetLeft) / canvas.width,
+      my = (e.pageY - canvas.offsetTop) / canvas.height,
+      json = JSON.stringify({'x': mx, 'y': my, 'hue': hue})
+
+  console.log('[post] /fire : ' + json)
+
+  fetch(location.origin + '/fire', {
+      method: 'post',
+      body: json
+  })
+}
+
 window.addEventListener('load', () => {
     let canvas = document.getElementById('night_sky')
 
     canvas.addEventListener('click',  e => {
-        e.preventDefault()
-
-        let mx = (e.pageX - canvas.offsetLeft) / canvas.width,
-            my = (e.pageY - canvas.offsetTop) / canvas.height,
-            json = JSON.stringify({'x': mx, 'y': my, 'hue': hue})
-
-        console.log('[post] /fire : ' + json)
-
-        fetch(location.origin + '/fire', {
-            method: 'post',
-            body: json
-        })
+        fire(canvas, e)
     })
 })
