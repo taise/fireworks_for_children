d9a4e0d7d6fb1a98dbae68c9eeaaa73bd50913ee
taise <taise515@gmail.com>
2017-12-08 17:34:13 +0900

    Displayサイズにかかわらず、同じ場所に花火を飛ばす

--- public/js/fire.js
+++ public/js/fire.js
@@ -4,8 +4,8 @@ window.addEventListener('load', () => {
     canvas.addEventListener('click',  e => {
         e.preventDefault()
 
-        let mx = e.pageX - canvas.offsetLeft,
-            my = e.pageY - canvas.offsetTop,
+        let mx = (e.pageX - canvas.offsetLeft) / canvas.width,
+            my = (e.pageY - canvas.offsetTop) / canvas.height,
             json = JSON.stringify({'x': mx, 'y': my})
 
         console.log('[post] /fire : ' + json)
