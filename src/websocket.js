ee415a7384d2fcd095f24478b6615259e1bc933a
taise <taise515@gmail.com>
2017-12-26 08:58:44 +0900

    Fix: eslint

--- src/websocket.js
+++ src/websocket.js
@@ -2,7 +2,7 @@ import { consoleLog } from './utils/console'
 
 export function createWebSocket (path) {
   let protocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
-  let ws = new WebSocket(protocol + '://' + window.location.host + path)
+  let ws = new window.WebSocket(protocol + '://' + window.location.host + path)
   ws.onopen = () => consoleLog('connection opened')
   ws.onclose = () => consoleLog('connection closed')
   return ws
