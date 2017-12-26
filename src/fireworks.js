c49c58804e2bc6cab8899a59afd5894dc8573211
taise <taise515@gmail.com>
2017-12-27 00:27:04 +0900

    Refactoring: particle.js

--- src/fireworks.js
+++ src/fireworks.js
@@ -1,4 +1,5 @@
 import { random } from './utils/random'
+import { Particle } from './particle'
 
 // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
 // not supported in all browsers though and sometimes needs a prefix, so we need a shim
@@ -115,61 +116,6 @@ Firework.prototype.draw = function () {
   ctx.stroke()
 }
 
-// create particle
-function Particle (x, y, hue) {
-  this.x = x
-  this.y = y
-  // track the past coordinates of each particle to create a trail effect, increase the coordinate count to create more prominent trails
-  this.coordinates = []
-  this.coordinateCount = 5
-  while (this.coordinateCount--) {
-    this.coordinates.push([this.x, this.y])
-  }
-  // set a random angle in all possible directions, in radians
-  this.angle = random(0, Math.PI * 2)
-  this.speed = random(1, 10)
-  // friction will slow the particle down
-  this.friction = 0.95
-  // gravity wilal be applied and pull the particle down
-  this.gravity = 0.95
-  // set the hue to a random number +-50 of the overall hue variable
-  this.hue = random(hue - 50, hue + 50)
-  this.brightness = random(50, 80)
-  this.alpha = 1
-  // set how fast the particle fades out
-  this.decay = random(0.015, 0.03)
-}
-
-// update particle
-Particle.prototype.update = function (index) {
-  // remove last item in coordinates array
-  this.coordinates.pop()
-  // add current coordinates to the start of the array
-  this.coordinates.unshift([this.x, this.y])
-  // slow down the particle
-  this.speed *= this.friction
-  // apply velocity
-  this.x += Math.cos(this.angle) * this.speed
-  this.y += Math.sin(this.angle) * this.speed + this.gravity
-  // fade out the particle
-  this.alpha -= this.decay
-
-  // remove the particle once the alpha is low enough, based on the passed in index
-  if (this.alpha <= this.decay) {
-    particles.splice(index, 1)
-  }
-}
-
-// draw particle
-Particle.prototype.draw = function () {
-  ctx.beginPath()
-  // move to the last tracked coordinates in the set, then draw a line to the current x and y
-  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1])
-  ctx.lineTo(this.x, this.y)
-  ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')'
-  ctx.stroke()
-}
-
 // create particle group/explosion
 function createParticles (x, y, hue) {
   // increase the particle count for a bigger explosion, beware of the canvas performance hit with the increased particles though
@@ -211,8 +157,11 @@ function loop () {
   // loop over each particle, draw it, update it
   var j = particles.length
   while (j--) {
-    particles[j].draw()
+    particles[j].draw(ctx)
     particles[j].update(j)
+    if (particles[j].isEnd()) {
+      particles.splice(j, 1)
+    }
   }
 }
 // once the window loads, we are ready for some fireworks!
