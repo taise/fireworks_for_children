544204b2121cc46332d72161b02146ee4dd24da2
taise <taise515@gmail.com>
2017-12-28 09:25:11 +0900

    Refactoring: global変数をEnvに持っていく

--- src/env.js
+++ src/env.js
@@ -2,9 +2,14 @@ import { random } from './utils/random'
 
 export class Env {
   constructor () {
+    this.canvas = document.getElementById('night_sky')
+    this.ctx = this.canvas.getContext('2d')
     this.hue = random(0, 360)
     this.cw = window.innerWidth
     this.ch = window.innerHeight
     this.limiterTotal = 1
+
+    this.canvas.width = this.cw
+    this.canvas.height = this.ch
   }
 }
--- src/fireworks.js
+++ src/fireworks.js
@@ -1,4 +1,5 @@
 import { Particle } from './particle'
+import { Env } from './env'
 
 // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
 // not supported in all browsers though and sometimes needs a prefix, so we need a shim
@@ -11,47 +12,26 @@ window.requestAnimFrame = (() => {
         }
 })()
 
-// now we will setup our basic variables for the demo
-const canvas = document.getElementById('night_sky')
-const cw = window.innerWidth
-const ch = window.innerHeight
-const particleCount = 30
-
-const ctx = canvas.getContext('2d')
-export const fireworks = []
-export const particles = []
-
-// set canvas dimensions
-canvas.width = cw
-canvas.height = ch
-
-// create particle group/explosion
 function createParticles (x, y, hue) {
-  // increase the particle count for a bigger explosion, beware of the canvas performance hit with the increased particles though
-  let count = particleCount
+  let count = 30
   while (count--) {
     particles.push(new Particle(x, y, hue))
   }
 }
 
-// main demo loop
-function loop () {
-  // this function will run endlessly with requestAnimationFrame
-  window.requestAnimFrame(loop)
+function clearRect (env) {
+  env.ctx.globalCompositeOperation = 'destination-out'
+  env.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
+  env.ctx.fillRect(0, 0, env.cw, env.ch)
+  env.ctx.globalCompositeOperation = 'lighter'
+}
 
-  // normally, clearRect() would be used to clear the canvas
-  // we want to create a trailing effect though
-  // setting the composite operation to destination-out will allow us to clear the canvas at a specific opacity, rather than wiping it entirely
-  ctx.globalCompositeOperation = 'destination-out'
-  // decrease the alpha property to create more prominent trails
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'
-  ctx.fillRect(0, 0, cw, ch)
-  // change the composite operation back to our main mode
-  // lighter creates bright highlight points as the fireworks and particles overlap each other
-  ctx.globalCompositeOperation = 'lighter'
+function loop () {
+  window.requestAnimFrame(loop)
+  clearRect(env)
 
   for (const [i, firework] of fireworks.entries()) {
-    firework.draw(ctx)
+    firework.draw(env.ctx)
     firework.update()
     if (firework.isSparkly()) {
       createParticles(firework)
@@ -60,14 +40,17 @@ function loop () {
     }
   }
 
-  // loop over each particle, draw it, update it
   for (const [j, particle] of particles.entries()) {
-    particle.draw(ctx)
+    particle.draw(env.ctx)
     particle.update()
     if (particle.isEnd()) {
       particles.splice(j, 1)
     }
   }
 }
-// once the window loads, we are ready for some fireworks!
+
+export const fireworks = []
+export const particles = []
+const env = new Env()
+
 window.onload = loop
